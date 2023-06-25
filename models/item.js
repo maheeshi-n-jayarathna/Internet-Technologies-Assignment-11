aa012032b36749868faad6262055eb4b12ee65f9
maheeshi-n-jayarathna <maheeshijayarathna@gmail.com>
2023-06-25 18:21:55 +0530

    added

--- models/item.js
+++ models/item.js
@@ -1,32 +1,40 @@
-export class Item{
-    constructor(item_code, item_name, item_price, item_quantity) {
+export class Item {
+    constructor(item_code, item_name, item_price, item_qty_on_stock) {
         this._item_code = item_code;
         this._item_name = item_name;
         this._item_price = item_price;
-        this._item_quantity = item_quantity;
+        this._item_qty_on_stock = item_qty_on_stock;
     }
-    get item_code(){
+
+    set item_code(item_code) {
+        this._item_code = item_code;
+    }
+
+    get item_code() {
         return this._item_code;
     }
-    get item_name(){
-        return this._item_name;
-    }
-    get item_price(){
-        return this._item_price;
-    }
-    get item_quantity(){
-        return this._item_quantity;
-    }
-    set item_code(item_code){
-        this._item_code = item_code;
-    }
-    set item_name(item_name){
+
+    set item_name(item_name) {
         this._item_name = item_name;
     }
-    set item_price(item_price){
+
+    get item_name() {
+        return this._item_name;
+    }
+
+    set item_price(item_price) {
         this._item_price = item_price;
     }
-    set item_quantity(item_quantity){
-        this._item_quantity = item_quantity;
+
+    get item_price() {
+        return this._item_price;
+    }
+
+    set item_qty_on_stock(item_quantity) {
+        this._item_qty_on_stock = item_quantity;
+    }
+
+    get item_qty_on_stock() {
+        return this._item_qty_on_stock;
     }
 }
